e08bd7ac63ab74b27783149b94584f73b1d625b8
hyun <edocnuyh@gmail.com>
2024-10-03 19:47:38 +0900

    test: 특강 신청 테스트 작성

--- src/test/java/com/hhplus/architecture/domain/lectureRegistration/LectureRegistrationServiceTest.java
+++ src/test/java/com/hhplus/architecture/domain/lectureRegistration/LectureRegistrationServiceTest.java
@@ -1,11 +1,17 @@
 package com.hhplus.architecture.domain.lectureRegistration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import com.hhplus.architecture.domain.lecture.Lecture;
+import com.hhplus.architecture.domain.lecture.LectureException;
+import com.hhplus.architecture.domain.lecture.LectureException.ErrorCode;
 import com.hhplus.architecture.domain.lecture.LectureRepository;
+import com.hhplus.architecture.domain.lectureRegistration.LectureRegistrationCommand.Regist;
 import com.hhplus.architecture.domain.lectureRegistration.LectureRegistrationCommand.Search;
+import com.hhplus.architecture.domain.lectureRegistration.LectureRegistrationException.RegistrationError;
 import com.hhplus.architecture.domain.student.StudentInfo;
 import com.hhplus.architecture.domain.student.StudentRepository;
 import java.time.LocalDateTime;
@@ -61,6 +67,71 @@ class LectureRegistrationServiceTest {
         assertEquals(studentId, result.studentInfo().id());
         // 2. 특강 사이즈 일치
         assertEquals(2, result.lectureInfos().size());
+    }
+
+    @Test
+    @DisplayName("기신청건이 있는 경우 ALREADY_REGISTERED 오류 발생")
+    void registButAlreadyRegisterd() {
+        // given
+        long lectureId = 1L;
+        long studentId = 1L;
+
+        // when
+        when(registrationRepository.countLectureRegistration(lectureId, studentId)).thenReturn(1);
+
+        // 검증
+        LectureRegistrationException exception = assertThrows(
+            LectureRegistrationException.class, () -> lectureRegistrationService.regist(
+                new Regist(lectureId, studentId)));
+
+        assertEquals(RegistrationError.ALREADY_REGISTERED, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("특강이 최대 수용인원을 넘으면 OVER_MAX_STUDENT 오류 발생 ")
+    void registButOverMaxStudent() {
+        // given
+        long lectureId = 1L;
+        long studentId = 1L;
+
+        // when
+        when(registrationRepository.countLectureRegistration(lectureId, studentId)).thenReturn(0);
+        when(studentRepository.findById(studentId)).thenReturn(new StudentInfo(studentId, "학생1"));
+        when(lectureRepository.findOneById(lectureId)).thenReturn(
+            new Lecture(lectureId, "특강1", "강연자1", LocalDateTime.of(2024, 11, 10, 14, 0),
+                Lecture.MAX_STUDENT));
+
+        // 검증
+        LectureException exception = assertThrows(
+            LectureException.class, () -> lectureRegistrationService.regist(
+                new Regist(lectureId, studentId)));
+
+        assertEquals(ErrorCode.OVER_MAX_STUDENT, exception.getErrorCode());
+    }
+
+    @Test
+    @DisplayName("특강 수강 신청 성공!")
+    void registAndSuccess() {
+        // given
+        long lectureId = 1L;
+        long studentId = 1L;
+        int appliedCount = 1;
+
+        // when
+        when(registrationRepository.countLectureRegistration(lectureId, studentId)).thenReturn(0);
+        when(studentRepository.findById(studentId)).thenReturn(new StudentInfo(studentId, "학생1"));
+        when(lectureRepository.findOneById(lectureId)).thenReturn(
+            new Lecture(lectureId, "특강1", "강연자1", LocalDateTime.of(2024, 11, 10, 14, 0),
+                appliedCount));
+
+        // 검증
+        RegistrationCompleted completed = lectureRegistrationService.regist(
+            new Regist(lectureId, studentId));
+        verify(registrationRepository).insertOne(lectureId, studentId);
+        verify(lectureRepository).updateLectureAppliedCount(lectureId, appliedCount + 1);
+
+        assertEquals(studentId, completed.studentInfo().id());
+        assertEquals(lectureId, completed.lectureInfo().id());
     }
 
 }
